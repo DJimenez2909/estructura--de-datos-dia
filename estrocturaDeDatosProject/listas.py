5ba2243054e0f8530fc012601406aedbf44aff72
Duban Jimenez <duvanj390@gmail.com>
2024-10-24 17:48:29 -0500

    agregamos todo los archivos a la nube

---
+++ estrocturaDeDatosProject/listas.py
@@ -0,0 +1,29 @@
+# listas
+print("---resultado----")
+lista = [1]
+print(lista)
+lista1 = [1,2,3,4,5,"hola", 4.5]
+print(lista1)
+print("--------------------------------")
+
+
+#enlazar las listas
+print("---resultado----")
+lista2 =[0,1,2,3]
+lista3 =["A","B","C"]
+lista4= [lista2,lista3]
+print(lista4)
+print(lista4[1][1])
+print("--------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
